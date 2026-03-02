4b3958e154c7be257beda75b2c76bf3f7ed57342
XanderAlmario <christian.almario@student.ateneo.edu>
2026-03-02 11:50:30 +0800

    Updated CardManager.gd and created Card.gd to handle hovering effect and dragging feedback

--- CardManager.gd
+++ CardManager.gd
@@ -4,6 +4,7 @@ const COLLISION_MASK_CARD = 1
 
 var draggingCard
 var screenSize
+var isHovering
 
 func _ready():
 	screenSize = get_viewport_rect().size
@@ -18,9 +19,43 @@ func _input(event):
 		if event.pressed:
 			var card = checkCard()
 			if card:
-				draggingCard = card
+				dragging(card)
 		else:
-			draggingCard = null
+			stopDragging()
+			
+func dragging(card):
+	card.scale = Vector2(1, 1)
+	draggingCard = card
+
+func stopDragging():
+	draggingCard.scale = Vector2(1.05, 1.05)
+	draggingCard = null
+
+func connectSignals(card):
+	card.connect("hovering", hoveringCard)
+	card.connect("stoppedHovering", stoppedHoveringCard)
+	
+func hoveringCard(card):
+	if !isHovering:
+		highlightCard(card, true)
+		isHovering = true
+	
+func stoppedHoveringCard(card):
+	if !draggingCard:
+		highlightCard(card, false)
+		var newCardHovering = checkCard()
+		if newCardHovering:
+			highlightCard(newCardHovering, true)
+		else:
+			isHovering = false
+	
+func highlightCard(card, hovering):
+	if hovering:
+		card.scale = Vector2(1.05, 1.05)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1, 1)
+		card.z_index = 1
 
 func checkCard():
 	var space_state = get_world_2d().direct_space_state
@@ -30,5 +65,18 @@ func checkCard():
 	parameters.collision_mask = COLLISION_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if (result.size() > 0):
-		return result[0].collider.get_parent()
+		#return result[0].collider.get_parent()
+		return highestZIndCard(result)
 	return null
+
+func highestZIndCard(cards):
+	var highestZCard = cards[0].collider.get_parent()
+	var highestZInd = highestZCard.z_index
+	
+	for i in range(1, cards.size()):
+		var currentCard = cards[i].collider.get_parent()
+		if currentCard.z_index > highestZInd:
+			highestZCard = currentCard
+			highestZInd = currentCard.z_index
+	
+	return highestZCard
