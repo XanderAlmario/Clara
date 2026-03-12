8e0f20d0f1c03677151d5ea366c1ce88e33d5602
Nico Acero <trisha.acero@student.ateneo.edu>
2026-03-12 21:40:49 +0800

    Added script for hero selection

---
+++ Scripts/hero_select.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_mage_select_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/match.tscn")
+
+
+func _on_monk_select_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/match.tscn")
+
+
+func _on_paladin_select_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/match.tscn")
+
+
+func _on_undead_select_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/match.tscn")
+
+
+func _on_archer_select_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/match.tscn")
