a42a723fbc2b7ea1090bd3f666c2c38f9894eb4e
p_ca <119797972+Sticity@users.noreply.github.com>
2026-04-04 21:58:09 +0800

    Networking V1 (1/2 upload)

---
+++ 1.0/Scenes/game_setup.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+const STARTING_HEALTH = 10
+
+func host_setup():
+	$PlayerHP.text = str(STARTING_HEALTH)
+	get_parent().get_node("EnemyField/EnemyHP").text = str(STARTING_HEALTH)
+	$BattleManager.playerHP = STARTING_HEALTH
+	$BattleManager.enemyHP = STARTING_HEALTH
+	
+	get_parent().get_node("EnemyField/EnemyDeck").deck_size = 8
+	get_parent().get_node("EnemyField/EnemyDeck/RichTextLabel").text = "8"
+	
+	await $Deck.draw_initial_hand()
+	
+	$EndTurnButton.visible = false
+	$EndTurnButton.disabled = true
+	
+	$InputManager.inputs_disabled = false
+	
+func client_setup():
+	$PlayerHP.text = str(STARTING_HEALTH)
+	get_parent().get_node("EnemyField/EnemyHP").text = str(STARTING_HEALTH)
+	$BattleManager.playerHP = STARTING_HEALTH
+	$BattleManager.enemyHP = STARTING_HEALTH
+	
+	get_parent().get_node("EnemyField/EnemyDeck").deck_size = 8
+	get_parent().get_node("EnemyField/EnemyDeck/RichTextLabel").text = "8"
+	
+	$Deck.draw_initial_hand()
